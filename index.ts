1f24f67b5e4d80dff27937d887a7d7ad6b7f8539
Anwesh43 <anweshthecool0@gmail.com>
2021-04-13 20:51:57 +0530

    adding colors

---
+++ index.ts
@@ -0,0 +1,15 @@
+const w : number = window.innerWidth 
+const h : number = window.innerHeight
+const parts : number = 4  
+const scGap : number = 0.02 / parts 
+const strokeFactor : number = 90 
+const sizeFactor : number = 2.9 
+const backColor : string = "#BDBDBD"
+const delay : number = 20 
+const colors : Array<string> = [
+    "#1abc9c",
+    "#8e44ad",
+    "#f39c12",
+    "#e74c3c",
+    "#27ae60"
+]
